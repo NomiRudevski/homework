4e85319cfe1038ec8fd5b2a373aa051b31662c0e
NomiRudevski <nomi.rudevski@gmail.com>
2024-05-16 11:18:20 +0300

    add

---
+++ app.py
@@ -0,0 +1,5 @@
+from helper import sum, mult
+
+if __name__ == "__main__":
+    sum(3,4)
+    mult(5,6)
---
+++ helper.py
@@ -0,0 +1,7 @@
+from icecream import ic
+
+def mult(num1=1, num2=3):
+    ic(num1*num2)
+
+def sum(num1=1, num2=2):
+    ic(num1+num2)
